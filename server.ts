e6046e4ceb1bef330e566395c213f746cd30d44e
Jesus <jes.librado@gmail.com>
2020-12-24 02:31:40 -0600

    Added nodemon and server file

---
+++ server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
+
+const app = express();
+const PORT = process.env.PORT;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at port ${PORT}`);
+});
