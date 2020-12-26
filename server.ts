a33cfac97ad4bb620de74445f0911f72f654b2c6
Jesus <jes.librado@gmail.com>
2020-12-26 02:50:40 -0600

    Added eslint and prettier rules

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
-import express from 'express';
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname+'/.env' });
+import express from 'express';
+
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 const PORT = process.env.PORT;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
